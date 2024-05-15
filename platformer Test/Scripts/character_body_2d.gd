8c9b3c54b91ba30feece8ebafc12873ca0a12a62
Maxwell Moss <159182842+Maxwelltheguy@users.noreply.github.com>
2024-05-15 13:27:07 -0400

    Update character_body_2d.gd

--- platformer Test/Scripts/character_body_2d.gd
+++ platformer Test/Scripts/character_body_2d.gd
@@ -4,10 +4,10 @@ extends CharacterBody2D
 const GRAVITY = 300
 
 func _input(event):
-	if event.is_action_pressed("left"):
+	if event.is_action("left"):
 		Move_Left()
 
 
 func Move_Left():
-	velocity.x = -20
+	velocity.x += -20
 
